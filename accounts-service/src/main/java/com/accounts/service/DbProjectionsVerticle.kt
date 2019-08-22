c944ddb829b5a22ef62d972c72e3501c7995c420
rodolfodpk <rodolfo_dpk@yahoo.com>
2019-08-21 23:58:03 -0300

    version aligned with crabzilla, Main methods moved to crabzilla-pgc

--- accounts-service/src/main/java/com/accounts/service/DbProjectionsVerticle.kt
+++ accounts-service/src/main/java/com/accounts/service/DbProjectionsVerticle.kt
@@ -2,24 +2,13 @@ package com.accounts.service
 
 
 import com.accounts.service.projectors.AccountsSummaryProjector
-import io.github.crabzilla.pgc.PgcComponent
-import io.github.crabzilla.pgc.PgcProjectionComponent
-import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
-import org.slf4j.LoggerFactory.getLogger
 
-class DbProjectionsVerticle : AbstractVerticle() {
-
-  private lateinit var pgcComponent : PgcComponent
-
-  companion object {
-    internal val log = getLogger(DbProjectionsVerticle::class.java)
-  }
+class DbProjectionsVerticle : PgcDbProjectionsVerticle() {
 
   override fun start(startFuture: Future<Void>) {
-    pgcComponent = PgcComponent(vertx, config())
-    val projectionComponent = PgcProjectionComponent(pgcComponent)
-    projectionComponent.addProjector("accounts-summary", AccountsSummaryProjector())
+    super.start(startFuture)
+    addProjector("accounts-summary", AccountsSummaryProjector())
     startFuture.complete()
   }
 
