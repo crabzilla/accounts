06e8243245819cb862b80d1591a44842f5e5de5e
rodolfodpk <rodolfo_dpk@yahoo.com>
2019-08-26 19:18:32 -0300

    reflecting latest changes from Crabzilla

--- accounts-service/src/main/java/com/accounts/service/AcctsUIPjcVerticle.kt
+++ accounts-service/src/main/java/com/accounts/service/AcctsUIPjcVerticle.kt
@@ -1,22 +1,33 @@
 package com.accounts.service
 
 
+import io.github.crabzilla.webpgc.DbProjectionsVerticle.Companion.amIAlreadyRunning
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
 import io.vertx.core.json.JsonObject
 import org.slf4j.LoggerFactory.getLogger
+import java.lang.management.ManagementFactory
 
 class AcctsUIPjcVerticle : AbstractVerticle() {
 
   companion object {
     internal val log = getLogger(AcctsUIPjcVerticle::class.java)
+    val processId: String = ManagementFactory.getRuntimeMXBean().name // TODO does this work with AOT?
   }
 
   override fun start(startFuture: Future<Void>) {
+    // publishes events to UI
     vertx.eventBus().consumer<String>(config().getString("PROJECTION_ENDPOINT")) { message ->
       val eventsAsJson = JsonObject(message.body())
       vertx.eventBus().publish(config().getString("UI_PROJECTION_ENDPOINT"), eventsAsJson.encodePrettily())
     }
+    // a simple mechanism to avoid deploying a HA singleton Verticle on another node
+    val projectionEndpoint = this::class.java.name
+    vertx.eventBus().consumer<String>(amIAlreadyRunning(projectionEndpoint)) { msg ->
+      log.info("received " + msg.body())
+      msg.reply("Yes, $projectionEndpoint is already running here: $processId")
+    }
+
     startFuture.complete()
   }
 
--- accounts-service/src/main/java/com/accounts/service/Main.kt
+++ accounts-service/src/main/java/com/accounts/service/Main.kt
@@ -34,13 +34,15 @@ object Main {
         getConfig(vertx, CONFIG_PATH).setHandler { gotConfig ->
           if (gotConfig.succeeded()) {
             val config = gotConfig.result()
-            val deploymentOptions = DeploymentOptions().setHa(true).setConfig(config)
+            val webOptions = DeploymentOptions().setHa(true).setConfig(config)
+            val backOptions = DeploymentOptions().setHa(true).setConfig(config).setInstances(1)
             val dbProjectionsEndpoint = config.getString("PROJECTION_ENDPOINT")
             CompositeFuture.all(
-              deploy(vertx, AcctsWebVerticle::class.java.name, deploymentOptions),
-              deploy(vertx, AcctsUIPjcVerticle::class.java.name, deploymentOptions),
-              deploySingleton(vertx, AccountsDbPjcVerticle::class.java.name, dbProjectionsEndpoint,
-                      deploymentOptions, processId))
+              deploy(vertx, AcctsWebVerticle::class.java.name, webOptions),
+              deploySingleton(vertx,
+                      AcctsUIPjcVerticle::class.java.name, AcctsUIPjcVerticle::class.java.name, backOptions, processId),
+              deploySingleton(vertx,
+                      AccountsDbPjcVerticle::class.java.name, dbProjectionsEndpoint, backOptions, processId))
               .setHandler { deploys ->
                 if (deploys.succeeded()) {
                   val deploymentIds = deploys.result().list<String>()
