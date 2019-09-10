9fbb6127d19f54ac6a1c4a4214d61c2d5c3d459b
rodolfodpk <rodolfo_dpk@yahoo.com>
2019-09-09 23:08:15 -0300

    code formatting

--- accounts-service/src/main/java/com/crabzilla/examples/accounts/model/AccountCmdHandlerFactory.kt
+++ accounts-service/src/main/java/com/crabzilla/examples/accounts/model/AccountCmdHandlerFactory.kt
@@ -5,8 +5,11 @@ import io.vertx.core.AsyncResult
 import io.vertx.core.Handler
 
 class AccountCmdHandlerFactory : EntityCommandHandlerFactory<AccountEntity> {
+
   override fun createHandler(cmdMetadata: CommandMetadata, command: Command, snapshot: Snapshot<AccountEntity>,
                              handler: Handler<AsyncResult<UnitOfWork>>): EntityCommandHandler<AccountEntity> {
+
     return AccountCmdHandler(cmdMetadata, command, snapshot, AccountCmdAware.acctStateBuilder, handler)
   }
+
 }
