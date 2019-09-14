690b894f4c54f4ecfd7d10ef5aeb5c43a5f68ffd
rodolfodpk <rodolfo_dpk@yahoo.com>
2019-09-14 00:33:28 -0300

    fix consistency reports

--- accounts-service/src/main/java/com/crabzilla/examples/accounts/service/reports/ConsistencyRepository.kt
+++ accounts-service/src/main/java/com/crabzilla/examples/accounts/service/reports/ConsistencyRepository.kt
@@ -1,9 +1,8 @@
 package com.crabzilla.examples.accounts.service.reports
 
-import io.vertx.core.AsyncResult
 import io.vertx.core.CompositeFuture
 import io.vertx.core.Future
-import io.vertx.core.Handler
+import io.vertx.core.Promise
 import io.vertx.core.json.JsonArray
 import io.vertx.core.json.JsonObject
 import io.vertx.pgclient.PgPool
@@ -20,38 +19,35 @@ class ConsistencyRepository(private val db: PgPool) {
    *  2. all accounts within write model that doesn't have it's respective summary (read model)
    *  3. all accounts with inconsistent balances
    */
-  fun inconsistencies(aHandler: Handler<AsyncResult<JsonObject>>) {
+  fun inconsistencies(): Promise<JsonObject> {
 
-    val readModelFuture: Future<JsonArray> = Future.future()
-    missingReadModel(readModelFuture)
+    val promise = Promise.promise<JsonObject>()
+    val result = JsonObject()
 
-    val writeModelFuture = Future.future<JsonArray>()
-    missingWriteModel(writeModelFuture)
+    val f1 = missingReadModel()
+    val f2 = missingWriteModel()
+    val f3 = balanceMismatch()
 
-    val mismatchFuture = Future.future<JsonArray>()
-    balanceMismatch(mismatchFuture)
-
-    CompositeFuture.all(readModelFuture, writeModelFuture, mismatchFuture).setHandler { event ->
-      if (event.succeeded()) {
-        val result = JsonObject()
-        val readModel = readModelFuture.result()
-        val writeModel = writeModelFuture.result()
-        val mismatch = mismatchFuture.result()
-        if (!readModel.isEmpty) result.put("missing_read_model", readModel)
-        if (!writeModel.isEmpty) result.put("missing_write_model", writeModel)
-        if (!mismatch.isEmpty) result.put("balance_mismatch", mismatch)
-        aHandler.handle(Future.succeededFuture(result))
-      } else {
-        aHandler.handle(Future.failedFuture(event.cause()))
+    CompositeFuture.all(f1, f2, f3)
+      .setHandler { event ->
+          if (event.succeeded()) {
+            if (!f1.result().isEmpty) result.put("missing_read_model", f1.result())
+            if (!f2.result().isEmpty) result.put("missing_write_model", f2.result())
+            if (!f3.result().isEmpty) result.put("balance_mismatch", f3.result())
+            promise.complete(result)
+          } else {
+            promise.fail(event.cause())
+          }
       }
-    }
 
+    return promise
   }
 
   /**
    * Get all accounts within read model that doesn't have it's respective snapshot (write model)
    */
-  private fun missingReadModel(aHandler: Handler<AsyncResult<JsonArray>>) {
+  private fun missingReadModel(): Future<JsonArray> {
+    val promise = Promise.promise<JsonArray>()
     val sql = """SELECT account_snapshots.ar_id,
                         (account_snapshots.json_content -> 'balance')::numeric
                    FROM account_snapshots
@@ -59,29 +55,31 @@ class ConsistencyRepository(private val db: PgPool) {
                  ORDER by ar_id """.trimMargin()
     db.preparedQuery(sql) { event ->
       if (event.failed()) {
-        aHandler.handle(Future.failedFuture(event.cause())); return@preparedQuery
+        promise.fail(event.cause()); return@preparedQuery
       }
       val set = event.result()
       val array = JsonArray()
       for (row in set) {
-        val jo = JsonObject().put("id", row.getInteger(0)).put("balance", row.getBigDecimal(1))
+        val jo = JsonObject().put("id", row.getInteger(0)).put("balance", row.getBigDecimal(1).toDouble())
         array.add(jo)
       }
-      aHandler.handle(Future.succeededFuture(array))
+      promise.complete(array)
     }
+    return promise.future()
   }
 
   /**
    * Get all accounts within write model that doesn't have it's respective summary (read model)
    */
-  private fun missingWriteModel(aHandler: Handler<AsyncResult<JsonArray>>) {
+  private fun missingWriteModel(): Future<JsonArray> {
+    val promise = Promise.promise<JsonArray>()
     val sql = """SELECT account_summary.id, account_summary.balance
                    FROM account_summary
                   WHERE id not in (SELECT ar_id from account_snapshots)
                  ORDER by id """.trimMargin()
     db.preparedQuery(sql) { event ->
       if (event.failed()) {
-        aHandler.handle(Future.failedFuture(event.cause())); return@preparedQuery
+        promise.fail(event.cause()); return@preparedQuery
       }
       val set = event.result()
       val array = JsonArray()
@@ -89,14 +87,16 @@ class ConsistencyRepository(private val db: PgPool) {
         val jo = JsonObject().put("id", row.getInteger(0)).put("balance", row.getBigDecimal(1))
         array.add(jo)
       }
-      aHandler.handle(Future.succeededFuture(array))
+      promise.complete(array)
     }
+    return promise.future()
   }
 
   /**
    * get all accounts with inconsistent balances
    */
-  private fun balanceMismatch(aHandler: Handler<AsyncResult<JsonArray>>) {
+  private fun balanceMismatch(): Future<JsonArray> {
+    val promise = Promise.promise<JsonArray>()
     val sql = """SELECT account_snapshots.ar_id,
                         (account_snapshots.json_content -> 'balance')::numeric as snapshot_balance,
                         account_summary.balance as summary_balance,
@@ -108,7 +108,7 @@ class ConsistencyRepository(private val db: PgPool) {
                  ORDER by difference """.trimMargin()
     db.preparedQuery(sql) { event ->
       if (event.failed()) {
-        aHandler.handle(Future.failedFuture(event.cause())); return@preparedQuery
+        promise.fail(event.cause()); return@preparedQuery
       }
       val set = event.result()
       val array = JsonArray()
@@ -117,8 +117,9 @@ class ConsistencyRepository(private val db: PgPool) {
                 .put("summary_balance", row.getBigDecimal(2)).put("difference", row.getBigDecimal(3))
         array.add(jo)
       }
-      aHandler.handle(Future.succeededFuture(array))
+      promise.complete(array)
     }
+    return promise.future()
   }
 
 }
--- accounts-service/src/main/java/com/crabzilla/examples/accounts/service/reports/ConsistencyWebHandlers.kt
+++ accounts-service/src/main/java/com/crabzilla/examples/accounts/service/reports/ConsistencyWebHandlers.kt
@@ -1,23 +1,22 @@
 package com.crabzilla.examples.accounts.service.reports
 
-import io.vertx.core.Handler
 import io.vertx.ext.web.RoutingContext
 
 class ConsistencyWebHandlers(private val consistencyRepo: ConsistencyRepository) {
 
   fun inconsistencies(rc: RoutingContext) {
-    consistencyRepo.inconsistencies(Handler { event ->
+    consistencyRepo.inconsistencies().future().setHandler { event ->
       if (event.failed()) {
-        rc.response().setStatusCode(500).setStatusMessage(event.cause().message).end(); return@Handler
+        rc.response().setStatusCode(500).setStatusMessage(event.cause().message).end(); return@setHandler
       }
       val result = event.result()
       if (result == null || result.isEmpty) {
         rc.response().setStatusCode(404).end("Both write and read models seems to be consistent, yay!");
-        return@Handler
+        return@setHandler
       }
       rc.response().putHeader("Content-Type", "application/json").setStatusCode(200).setChunked(true)
               .end(result.encode())
-    })
+    }
   }
 
 }
